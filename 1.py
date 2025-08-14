1dcddfda6ec6a9d455ef22743171a09e949fbbc6
bitsfuture <bitsfuture@gmail.com>
2025-08-14 11:13:30 +0530

    add files

---
+++ 1.py
@@ -0,0 +1,16 @@
+def add():
+    a=int(input("Enter a number"))
+    b=int(input("Enter a number"))
+    c=a+b
+    return c
+
+def sub():
+    a=int(input("Enter a number"))
+    b=int(input("Enter a number"))
+    c=a-b
+    return c
+
+d=add()
+print("the result is",d)
+e=sub()
+print("the result is",e)
